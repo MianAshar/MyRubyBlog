183dc1f1bd08d7166554ff3493a2bfd43962de20
Mian Ashar <ashar617@gmail.com>
2015-11-13 02:19:56 +0500

    update

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
 module MyRubyBlog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -22,7 +23,6 @@ module MyRubyBlog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
 
   end
 end
