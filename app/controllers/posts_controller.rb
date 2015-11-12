a0f8239a42137b7785a7f4ae8b352c9a9ebf31cd
Mian Ashar <ashar617@gmail.com>
2015-11-12 11:49:33 +0500

    Database

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+	def index 
+		@content_first = "This is a sample text for our awesome ruby blog"
+		@content_second = "This is a sample text for our awesome ruby blog"
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+
+
+end
